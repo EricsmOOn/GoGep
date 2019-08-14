c3c23e7879fb240ffc6196a04d6deff999b729b5
Eric <ericwongmoon@outlook.com>
2019-08-14 21:19:01 +0800

    update

--- gep/gene.go
+++ gep/gene.go
@@ -1,6 +1,7 @@
 package gep
 
 import (
+	"math"
 	"math/rand"
 	"time"
 )
@@ -10,7 +11,7 @@ type Gene struct {
 	Fitness float64 //适应度
 }
 
-func creatRandomGene(headLength int, tailLength int, funSet []byte, termSet []byte) Gene {
+func creatRandomGene(numOfGenes int, headLength int, tailLength int, funSet []byte, termSet []byte) *Gene {
 	r := rand.New(rand.NewSource(time.Now().UnixNano())) //初始化随机数
 
 	funSetNum := len(funSet)
@@ -18,26 +19,60 @@ func creatRandomGene(headLength int, tailLength int, funSet []byte, termSet []by
 
 	set := append(funSet, termSet...)
 
-	gene := &Gene{make([]byte, 0), 0}
+	gene := Gene{make([]byte, 0), 0}
 
-	for i := 0; i < headLength; i++ {
-		gene.Gene = append(gene.Gene, set[r.Intn(funSetNum+termSetNum)])
+	for k := 0; k < numOfGenes; k++ {
+
+		for i := 0; i < headLength; i++ {
+			gene.Gene = append(gene.Gene, set[r.Intn(funSetNum+termSetNum)])
+		}
+
+		//fmt.Println(*(*string)(unsafe.Pointer(&gene.Gene)))
+
+		for i := 0; i < tailLength; i++ {
+			gene.Gene = append(gene.Gene, termSet[r.Intn(termSetNum)])
+		}
 	}
 
-	//fmt.Println(*(*string)(unsafe.Pointer(&gene.Gene)))
-
-	for i := 0; i < tailLength; i++ {
-		gene.Gene = append(gene.Gene, termSet[r.Intn(termSetNum)])
-	}
-
-	return *gene
+	return &gene
 }
 
-func CreatGenes(num int, headLength int, tailLength int, funSet []byte, termSet []byte) (genes []Gene) {
+func CreatGenes(numOfGenes int, num int, headLength int, tailLength int, funSet []byte, termSet []byte) []*Gene {
 
+	var genes []*Gene
+	var gene *Gene
 	for i := 0; i < num; i++ {
-		gene := creatRandomGene(headLength, tailLength, funSet, termSet)
+		gene = creatRandomGene(numOfGenes, headLength, tailLength, funSet, termSet)
 		genes = append(genes, gene)
 	}
 	return genes
+}
+
+func CalculateFitness(genes []*Gene, termSet []byte, mM int) {
+	testData := ReadTestData()
+	var termVarSet []float64
+
+	for _, gene := range genes {
+		//解码
+		g := Operate(*gene, termSet)
+
+		for _, td := range testData {
+			//求个体适应度
+
+			//逐个读入测试数据
+			termVarSet = td.TermVarSet
+
+			//求表达 result
+			result, err := Calculate(g, termVarSet, termSet)
+			if err == nil {
+				fi := float64(mM) - (math.Abs((result-td.Result)/td.Result) * 100)
+				if fi > 0 {
+					gene.Fitness += fi
+				}
+				//fmt.Print(" - ")
+				//fmt.Println(err)
+			}
+		}
+	}
+
 }
--- gepStarter.go
+++ gepStarter.go
@@ -3,19 +3,19 @@ package main
 import (
 	"fmt"
 	"gep-go/gep"
-	"math"
 	"unsafe"
 )
 
-//头部长度
-const HeadLength int = 3
-
 func main() {
 
+	//头部长度
+	headLength := 7
 	//基因产生个数
-	generateGenesNum := 7
+	populationsSize := 20
 	//选择范围
 	mM := 100
+	//染色体含有基因数
+	numOfGenes := 1
 	//误差精度
 
 	//函数集
@@ -25,48 +25,17 @@ func main() {
 	//最大操作数(参数个数)
 	maxFactorNum := gep.GetMaxFactorNum(funSet)
 	//基因尾部长度
-	tailLength := HeadLength*(maxFactorNum-1) + 1
+	tailLength := headLength*(maxFactorNum-1) + 1
 
-	genes := gep.CreatGenes(generateGenesNum, HeadLength, tailLength, funSet, termSet)
+	genes := gep.CreatGenes(numOfGenes, populationsSize, headLength, tailLength, funSet, termSet)
 
-	testData := gep.ReadTestData()
-	var termVarSet []float64
+	gep.CalculateFitness(genes, termSet, mM)
 
 	for n, gene := range genes {
+		//显示结果
 		fmt.Print(*(*string)(unsafe.Pointer(&gene.Gene))) //高效转换byte到String
-		fmt.Printf("-[%d]", n)
-
-		//解码
-		g := gep.Operate(gene, termSet)
-
-		for _, td := range testData {
-			//求个体适应度
-
-			//逐个读入测试数据
-			termVarSet = td.TermVarSet
-
-			//求表达 result
-			result, err := gep.Calculate(g, termVarSet, termSet)
-			if err == nil {
-				fi := float64(mM) - (math.Abs((result-td.Result)/td.Result) * 100)
-				if fi > 0 {
-					gene.Fitness += fi
-				}
-				//fmt.Print(" - ")
-				//fmt.Println(err)
-			}
-			////显示结果
-			//fmt.Print(" = ")
-			//fmt.Print(result)
-			////显示解析出的中缀表达式
-			//fmt.Print(" --> ")
-			//fmt.Println(*(*string)(unsafe.Pointer(&g)))
-		}
+		fmt.Printf("-[%2d]", n)
 		fmt.Print(" = ")
-		fmt.Print(gene.Fitness)
-		fmt.Print(" --> ")
-		fmt.Print(*(*string)(unsafe.Pointer(&g)))
-		fmt.Println()
+		fmt.Println(gene.Fitness)
 	}
-
 }
