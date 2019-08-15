c04b5d4f48603545815b37d59bb20d42310ff49b
Eric <ericwongmoon@outlook.com>
2019-08-15 12:13:31 +0800

    update 增加了变异.

--- gep/gene.go
+++ gep/gene.go
@@ -6,31 +6,32 @@ import (
 	"time"
 )
 
+var R *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 type Gene struct {
-	Gene    []byte  //基因序列
-	Fitness float64 //适应度
+	Gene       []byte  //基因序列
+	Fitness    float64 //适应度
+	Generation int     //代数
 }
 
-func creatRandomGene(numOfGenes int, headLength int, tailLength int, funSet []byte, termSet []byte) *Gene {
-	r := rand.New(rand.NewSource(time.Now().UnixNano())) //初始化随机数
-
+func creatRandomGene(generation int, numOfGenes int, headLength int, tailLength int, funSet []byte, termSet []byte) *Gene {
 	funSetNum := len(funSet)
 	termSetNum := len(termSet)
 
 	set := append(funSet, termSet...)
 
-	gene := Gene{make([]byte, 0), 0}
+	gene := Gene{make([]byte, 0), 0, generation}
 
 	for k := 0; k < numOfGenes; k++ {
 
 		for i := 0; i < headLength; i++ {
-			gene.Gene = append(gene.Gene, set[r.Intn(funSetNum+termSetNum)])
+			gene.Gene = append(gene.Gene, set[R.Intn(funSetNum+termSetNum)])
 		}
 
 		//fmt.Println(*(*string)(unsafe.Pointer(&gene.Gene)))
 
 		for i := 0; i < tailLength; i++ {
-			gene.Gene = append(gene.Gene, termSet[r.Intn(termSetNum)])
+			gene.Gene = append(gene.Gene, termSet[R.Intn(termSetNum)])
 		}
 	}
 
@@ -42,10 +43,51 @@ func CreatGenes(numOfGenes int, num int, headLength int, tailLength int, funSet 
 	var genes []*Gene
 	var gene *Gene
 	for i := 0; i < num; i++ {
-		gene = creatRandomGene(numOfGenes, headLength, tailLength, funSet, termSet)
+		gene = creatRandomGene(0, numOfGenes, headLength, tailLength, funSet, termSet)
 		genes = append(genes, gene)
 	}
 	return genes
+}
+
+func Evolution(genetic *Gene, numOfGenes int, num int, headLength int, tailLength int, funSet []byte, termSet []byte) []*Gene {
+	var genes []*Gene
+	var gene *Gene
+	genetic.Generation += 1
+	genetic.Fitness = 0
+	genes = append(genes, genetic)
+	for i := 0; i < num-1; i++ {
+		gene = creatRandomGene(genetic.Generation, numOfGenes, headLength, tailLength, funSet, termSet)
+		genes = append(genes, gene)
+	}
+	return genes
+}
+
+func Select(genes []*Gene) *Gene {
+	fitness := float64(0)
+	for _, gene := range genes {
+		fitness += gene.Fitness
+	}
+	f := R.Float64() * fitness
+	for _, gene := range genes {
+		f -= gene.Fitness
+		if f <= 0 {
+			return gene
+		}
+	}
+	return nil
+}
+
+func Mutation(mutationRate float64, gene *Gene, headLength, geneLength int, funSet, termSet []byte) *Gene {
+	funSet = append(funSet, termSet...)
+	if R.Float64() < mutationRate {
+		intn := R.Intn(len(gene.Gene))
+		if intn%geneLength < headLength {
+			gene.Gene[intn] = funSet[R.Intn(len(funSet))]
+		} else {
+			gene.Gene[intn] = termSet[R.Intn(len(termSet))]
+		}
+	}
+	return gene
 }
 
 func CalculateFitness(connectFun byte, geneLength int, numOfGenes int, genes []*Gene, termSet []byte, mM float64) {
--- gepStarter.go
+++ gepStarter.go
@@ -13,7 +13,7 @@ func main() {
 	//基因产生个数
 	populationsSize := 20
 	//选择范围
-	mM := float64(100)
+	selectRang := float64(100)
 	//染色体含有基因数
 	numOfGenes := 3
 	//连接函数
@@ -22,6 +22,8 @@ func main() {
 	funSet := []byte{'+', '-', '*', '/'}
 	//终点集
 	termSet := []byte{'a'}
+	//变异率
+	mutationRate := 0.5
 	//最大操作数(参数个数)
 	maxFactorNum := gep.GetMaxFactorNum(funSet)
 	//基因尾部长度
@@ -29,42 +31,65 @@ func main() {
 	//基因长度
 	geneLength := headLength + tailLength
 
-	//test(populationsSize,numOfGenes,geneLength,headLength,tailLength,funSet,termSet,connectFun,mM)
+	//1.寻找的最小适应度
+	//test(999,populationsSize,numOfGenes,geneLength,headLength,tailLength,funSet,termSet,connectFun,mutationRate,selectRang)
+
+	//1.寻找的最小适应度2.执行多少代以内
+	test2(999, 10, populationsSize, numOfGenes, geneLength, headLength, tailLength, funSet, termSet, connectFun, mutationRate, selectRang)
 
 	//1.最小适应度 2.最大适应度 3.需要的个数
-	findFintness(999.99, 1000, 10, populationsSize, numOfGenes, geneLength, headLength, tailLength, funSet, termSet, connectFun, mM)
+	//findFintness(999, 1000, 10, populationsSize, numOfGenes, geneLength, headLength, tailLength, funSet, termSet, connectFun, selectRang)
 
 }
 
-func test(populationsSize, numOfGenes, geneLength, headLength, tailLength int, funSet, termSet []byte, connectFun byte, mM float64) {
+func test(fitness float64, populationsSize, numOfGenes, geneLength, headLength, tailLength int, funSet, termSet []byte, connectFun byte, mutationRate, selectRang float64) {
+	flag := true
 	genes := gep.CreatGenes(numOfGenes, populationsSize, headLength, tailLength, funSet, termSet)
-	gep.CalculateFitness(connectFun, geneLength, numOfGenes, genes, termSet, mM)
-	for n, gene := range genes {
-		//显示结果
-		fmt.Print(*(*string)(unsafe.Pointer(&gene.Gene))) //高效转换byte到String
-		fmt.Printf("-[%2d]", n)
-		fmt.Print(" = ")
-		fmt.Print(gene.Fitness)
+	for {
+		gep.CalculateFitness(connectFun, geneLength, numOfGenes, genes, termSet, selectRang)
+		fmt.Printf("Geneation %d : \n", genes[0].Generation)
+		for n, gene := range genes {
+			if gene.Fitness > fitness {
+				flag = false
+			}
+			//显示结果
+			fmt.Print(*(*string)(unsafe.Pointer(&gene.Gene))) //高效转换byte到String
+			fmt.Printf("-[%2d]", n)
+			fmt.Print(" = ")
+			fmt.Print(gene.Fitness)
 
-		////显示中缀表达式
-		//fmt.Print(" --> ")
-		//k := gep.Operate(geneLength, numOfGenes, *gene, termSet)
-		//for i := 0; i < len(k); i++ {
-		//	fmt.Print(string(k[i]))
-		//	if i < len(k)-1 {
-		//		fmt.Printf(" %s ", string(connectFun))
-		//	}
-		//}
+			////显示中缀表达式
+			//fmt.Print(" --> ")
+			//k := gep.Operate(geneLength, numOfGenes, *gene, termSet)
+			//for i := 0; i < len(k); i++ {
+			//	fmt.Print(string(k[i]))
+			//	if i < len(k)-1 {
+			//		fmt.Printf(" %s ", string(connectFun))
+			//	}
+			//}
 
+			fmt.Println()
+		}
+		if !flag {
+			return
+		}
+		mutation := gep.Mutation(mutationRate, gep.Select(genes), headLength, geneLength, funSet, termSet)
+		//显示遗传个体
 		fmt.Println()
+		fmt.Print("遗传Gene:  ")
+		fmt.Print(*(*string)(unsafe.Pointer(&mutation.Gene))) //高效转换byte到String
+		fmt.Print(" = ")                                      //高效转换byte到String
+		fmt.Println(mutation.Fitness)
+		fmt.Println()
+		genes = gep.Evolution(mutation, numOfGenes, populationsSize, headLength, tailLength, funSet, termSet)
 	}
 }
 
-func findFintness(minFitness, maxFitness float64, num, populationsSize, numOfGenes, geneLength, headLength, tailLength int, funSet, termSet []byte, connectFun byte, mM float64) {
+func findFintness(minFitness, maxFitness float64, num, populationsSize, numOfGenes, geneLength, headLength, tailLength int, funSet, termSet []byte, connectFun byte, selectRang float64) {
 	l := 0
 	for {
 		genes := gep.CreatGenes(numOfGenes, populationsSize, headLength, tailLength, funSet, termSet)
-		gep.CalculateFitness(connectFun, geneLength, numOfGenes, genes, termSet, mM)
+		gep.CalculateFitness(connectFun, geneLength, numOfGenes, genes, termSet, selectRang)
 		for n, gene := range genes {
 			if gene.Fitness > minFitness && gene.Fitness < maxFitness {
 				//显示结果
@@ -88,6 +113,35 @@ func findFintness(minFitness, maxFitness float64, num, populationsSize, numOfGen
 					return
 				}
 			}
+		}
+	}
+}
+
+func test2(fitness float64, geneation, populationsSize, numOfGenes, geneLength, headLength, tailLength int, funSet, termSet []byte, connectFun byte, mutationRate, selectRang float64) {
+outfor:
+	for {
+		genes := gep.CreatGenes(numOfGenes, populationsSize, headLength, tailLength, funSet, termSet)
+		for {
+			gep.CalculateFitness(connectFun, geneLength, numOfGenes, genes, termSet, selectRang)
+			for _, gene := range genes {
+				if gene.Generation >= geneation {
+					goto outfor
+				}
+				if gene.Fitness > fitness && gene.Generation < geneation {
+					fmt.Printf("Geneation %d : \n", genes[0].Generation)
+					for n, gene := range genes {
+						fmt.Print(*(*string)(unsafe.Pointer(&gene.Gene))) //高效转换byte到String
+						fmt.Printf("-[%2d]", n)
+						fmt.Print(" = ")
+						fmt.Print(gene.Fitness)
+						fmt.Println()
+					}
+					return
+				}
+				//显示结果
+			}
+			mutation := gep.Mutation(mutationRate, gep.Select(genes), headLength, geneLength, funSet, termSet)
+			genes = gep.Evolution(mutation, numOfGenes, populationsSize, headLength, tailLength, funSet, termSet)
 		}
 	}
 }
